654d5dce490c428f1de87aeeb99d171c29e1e07a
Nitesh Kumar <82534120+Nitesh8877@users.noreply.github.com>
2025-05-13 23:27:04 +0530

    Update LandingPage.js

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -2,50 +2,53 @@ import React, { useEffect, useState } from 'react'
 import { getAllMovies } from '../../api/movie'
 import Navbar from '../../componets/Navbar/Navbar'
 import { Link } from 'react-router-dom'
-const LandingPage = () => {
 
+const LandingPage = () => {
     const [movieList, setMovieList] = useState([])
+    const [filteredMovies, setFilteredMovies] = useState([]) // added
     const [pageLoading, setPageLoading] = useState(true)
 
     const init = async () => {
         const result = await getAllMovies()
         setMovieList(result.data)
+        setFilteredMovies(result.data) // added
         setPageLoading(false)
     }
 
-    const selectedMovie = (movieName) => {
-        movieList.findIndex(function (movie) {
-            return movie.name.toUpperCase() === movieName.toUpperCase()
-        });
+    const selectedMovie = (searchText) => {
+        const filtered = movieList.filter((movie) =>
+            movie.name.toLowerCase().includes(searchText.toLowerCase())
+        )
+        setFilteredMovies(filtered)
     }
 
     useEffect(() => { init() }, [])
 
     return (
-        !pageLoading ? (<div>
-            <Navbar movies={movieList.map((movie) => movie.name)} onMovieSelect={selectedMovie} />
-            <div className='container mx-5 my-2'>
-                <p className='fw-bloder'>Recomended Movies</p>
-                <div className='row'>
-                    {
-                        movieList.map((movie) => (
-                            <div className="col-lg-3 col-xs-6  my-2" key={movie._id}>
-                                <Link key={movie._id} to={`/movie/${movie._id}/details`}>
-                                    <div className="d-flex align-items-stretch" style={{ height: 28 + 'rem' }}>
-                                        <div className="card bg-dark shadow-lg" style={{ width: 14 + "rem" }}>
-                                            <img src={movie.posterUrl} className="card-img-top" alt="..." style={{ height: '100%'}} />
-                                            <i className="bi bi-hand-thumbs-up-fill text-success px-2 " >58k </i>
-                                            <p className="text-white fw-bolder px-2">{movie.name}</p>
+        !pageLoading ? (
+            <div>
+                <Navbar movies={movieList.map((movie) => movie.name)} onMovieSelect={selectedMovie} />
+                <div className='container mx-5 my-2'>
+                    <p className='fw-bloder'>Recomended Movies</p>
+                    <div className='row'>
+                        {
+                            filteredMovies.map((movie) => (
+                                <div className="col-lg-3 col-xs-6  my-2" key={movie._id}>
+                                    <Link key={movie._id} to={`/movie/${movie._id}/details`}>
+                                        <div className="d-flex align-items-stretch" style={{ height: 28 + 'rem' }}>
+                                            <div className="card bg-dark shadow-lg" style={{ width: 14 + "rem" }}>
+                                                <img src={movie.posterUrl} className="card-img-top" alt="..." style={{ height: '100%' }} />
+                                                <i className="bi bi-hand-thumbs-up-fill text-success px-2" >58k </i>
+                                                <p className="text-white fw-bolder px-2">{movie.name}</p>
+                                            </div>
                                         </div>
-                                    </div>
-                                </Link>
-                            </div>
-
-                        ))
-                    }
+                                    </Link>
+                                </div>
+                            ))
+                        }
+                    </div>
                 </div>
             </div>
-        </div>
         ) : <div>Fetching Movies from backend ...</div>
     )
 }
