a424cf9442a85d9a70deeafc283d43c0eb164528
Nitesh Kumar <82534120+Nitesh8877@users.noreply.github.com>
2025-05-14 00:00:25 +0530

    Update MovieDetails.js

--- src/pages/MovieDetails/MovieDetails.js
+++ src/pages/MovieDetails/MovieDetails.js
@@ -1,93 +1,110 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from '../../componets/Navbar/Navbar'
 import ReactPlayer from 'react-player'
-import {useParams,Link} from 'react-router-dom'
-import {getMoviesById} from '../../api/movie/index'
-import {getAllTheaters} from '../../api/theatre/index'
-const  MovieDetails=()=> {
+import { useParams, Link } from 'react-router-dom'
+import { getMoviesById } from '../../api/movie/index'
+import { getAllTheaters } from '../../api/theatre/index'
 
-  const {movieid:id}=useParams();
-  const [selectedMovie,setSelectedMovie]=useState(id);
-  const [movieDetails,setMovieDetails]=useState({});
-  const [releaseStatus,setMovieReleaseStatus]=useState(false);
-  const [movieCast,setMovieCast]=useState([]);
-  const [isLoggedin,setLoggedin]=useState(false);
+const MovieDetails = () => {
 
-   
-  const init=async()=>{
+    const { movieid: id } = useParams()
+    const [selectedMovie, setSelectedMovie] = useState(id)
+    const [movieDetails, setMovieDetails] = useState({})
+    const [releaseStatus, setMovieReleaseStatus] = useState(false)
+    const [movieCast, setMovieCast] = useState([])
+    const [isLoggedin, setLoggedin] = useState(false)
 
-    try {
-      const results=await getAllTheaters();
-      if(results){
-        setLoggedin(true);
-      }
-    } catch (error) {
-      setLoggedin(false);
-    }
-    const response=await getMoviesById(selectedMovie);
-   
-    setMovieDetails(response.data[0]);
-    setMovieReleaseStatus(response.data[0].releaseStatus=='RELEASED')
-    setMovieCast(response.data[0].casts)
-
-
-  }
-  console.log(movieDetails)
-  
-  useEffect(()=>{
-    init();
-  },[])
-
-  const render=()=>{
-  return (
-   <>
-   <Navbar/><br></br>
-   <div className='bg-light'>
-    <div className='box bg-black backg'>
-      <ReactPlayer url={movieDetails.trailerUrl} controls={true} className="video" width="100%" height="300px" />
-    </div>
-    <div className='container my-4 justify-content-center'>
-      <div className='row'>
-        <div className='col'>
-          <img src={movieDetails.posterUrl} className='card' width={300} height={450} alt='...' />
-        </div>
-        <div className='col'>
-          <h2 className='fw-bolder'>About The Movie</h2>
-          <div className='d-flex'>
-            <span className='badge rounded-fill text-bg-danger m-1'>{movieDetails.description}</span>
-            <span className='dot my-auto'></span>
-            <span className='badge rounded-fill text-bg-secondary m-1'>{movieDetails.language}</span>
-           
-            <span className='badge rounded-fill text-bg-secondary m-1'>{movieDetails.releaseStatus}</span>
-           
-          </div>
-          <hr/>
-          <h3>Movie Name: {movieDetails.name}</h3>
-          <h3>Director: {movieDetails.director}</h3>
-          <h3>ReleaseDate: {movieDetails.releaseDate}</h3>
-          <hr/>
-        <h5>Cast</h5>
-        {
-          movieCast.map(name=><li key={name} className='list-group-item'>{name}</li>)
+    const init = async () => {
+        try {
+            const results = await getAllTheaters()
+            if (results) {
+                setLoggedin(true)
+            }
+        } catch (error) {
+            setLoggedin(false)
         }
-        <hr/>
-          <div className='text-center my-3' style={{display:isLoggedin?"block":'none'}}><Link key={selectedMovie} className='text-decoration-none btn btn-lg btn-success text-center' to={releaseStatus?`/buytickets/${movieDetails.name}/${selectedMovie}`:`#`}> {releaseStatus?"BOOk TICKET":"COMMING SOON"}</Link></div>
-          {
-            !isLoggedin?"Please login then book the ticket":""
-          }
-        </div>
-        
-      </div>
-    </div>
-   </div>
-   </>
-  )
-  }
+        const response = await getMoviesById(selectedMovie)
+        setMovieDetails(response.data[0])
+        setMovieReleaseStatus(response.data[0].releaseStatus === 'RELEASED')
+        setMovieCast(response.data[0].casts)
+    }
 
-  return (
-    render()
-  )
+    useEffect(() => {
+        init()
+    }, [])
+
+    return (
+        <>
+            <Navbar />
+            <div className="bg-dark text-white">
+                <div className="container py-4">
+                    {/* Trailer Section */}
+                    <div className="mb-4">
+                        <ReactPlayer
+                            url={movieDetails.trailerUrl}
+                            controls={true}
+                            width="100%"
+                            height="350px"
+                            className="shadow-lg rounded"
+                        />
+                    </div>
+
+                    <div className="row">
+                        {/* Movie Poster */}
+                        <div className="col-lg-4 col-md-6 mb-4 text-center">
+                            <img
+                                src={movieDetails.posterUrl}
+                                alt={movieDetails.name}
+                                className="img-fluid rounded shadow-lg"
+                                style={{ maxHeight: '450px', objectFit: 'cover' }}
+                            />
+                        </div>
+
+                        {/* Movie Information */}
+                        <div className="col-lg-8 col-md-6">
+                            <h2 className="fw-bold">{movieDetails.name}</h2>
+                            <p className="text-muted">{movieDetails.description}</p>
+                            <div className="d-flex flex-wrap mb-3">
+                                <span className="badge bg-danger m-1">{movieDetails.language}</span>
+                                <span className="badge bg-secondary m-1">{movieDetails.releaseStatus}</span>
+                            </div>
+
+                            <div>
+                                <h5 className="fw-semibold">Director: {movieDetails.director}</h5>
+                                <h5 className="fw-semibold">Release Date: {movieDetails.releaseDate}</h5>
+                            </div>
+
+                            {/* Cast List */}
+                            <div className="my-3">
+                                <h5 className="fw-semibold">Cast:</h5>
+                                <ul className="list-group">
+                                    {movieCast.map((name, index) => (
+                                        <li key={index} className="list-group-item bg-dark text-white border-0">
+                                            {name}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+
+                            {/* Booking Button */}
+                            <div className="text-center my-4">
+                                {isLoggedin ? (
+                                    <Link
+                                        to={releaseStatus ? `/buytickets/${movieDetails.name}/${selectedMovie}` : '#'}
+                                        className={`btn btn-lg ${releaseStatus ? 'btn-success' : 'btn-secondary'} text-white`}
+                                    >
+                                        {releaseStatus ? 'BOOK TICKET' : 'COMING SOON'}
+                                    </Link>
+                                ) : (
+                                    <p className="text-warning">Please login to book tickets.</p>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
-
 
 export default MovieDetails
