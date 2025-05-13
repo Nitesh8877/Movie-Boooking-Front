b3054d83d8a801b4c2caf00d135bfdee732657aa
Nitesh Kumar <82534120+Nitesh8877@users.noreply.github.com>
2025-05-13 23:29:29 +0530

    Update LandingPage.js

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom'
 
 const LandingPage = () => {
     const [movieList, setMovieList] = useState([])
-    const [filteredMovies, setFilteredMovies] = useState([]) // added
+    const [filteredMovies, setFilteredMovies] = useState([])
     const [pageLoading, setPageLoading] = useState(true)
 
     const init = async () => {
         const result = await getAllMovies()
         setMovieList(result.data)
-        setFilteredMovies(result.data) // added
+        setFilteredMovies(result.data)
         setPageLoading(false)
     }
 
@@ -29,22 +29,26 @@ const LandingPage = () => {
             <div>
                 <Navbar movies={movieList.map((movie) => movie.name)} onMovieSelect={selectedMovie} />
                 <div className='container mx-5 my-2'>
-                    <p className='fw-bloder'>Recomended Movies</p>
+                    <p className='fw-bloder'>Recommended Movies</p>
                     <div className='row'>
                         {
-                            filteredMovies.map((movie) => (
-                                <div className="col-lg-3 col-xs-6  my-2" key={movie._id}>
-                                    <Link key={movie._id} to={`/movie/${movie._id}/details`}>
-                                        <div className="d-flex align-items-stretch" style={{ height: 28 + 'rem' }}>
-                                            <div className="card bg-dark shadow-lg" style={{ width: 14 + "rem" }}>
-                                                <img src={movie.posterUrl} className="card-img-top" alt="..." style={{ height: '100%' }} />
-                                                <i className="bi bi-hand-thumbs-up-fill text-success px-2" >58k </i>
-                                                <p className="text-white fw-bolder px-2">{movie.name}</p>
+                            filteredMovies.length > 0 ? (
+                                filteredMovies.map((movie) => (
+                                    <div className="col-lg-3 col-xs-6 my-2" key={movie._id}>
+                                        <Link to={`/movie/${movie._id}/details`}>
+                                            <div className="d-flex align-items-stretch" style={{ height: '28rem' }}>
+                                                <div className="card bg-dark shadow-lg" style={{ width: '14rem' }}>
+                                                    <img src={movie.posterUrl} className="card-img-top" alt="..." style={{ height: '100%' }} />
+                                                    <i className="bi bi-hand-thumbs-up-fill text-success px-2">58k</i>
+                                                    <p className="text-white fw-bolder px-2">{movie.name}</p>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </Link>
-                                </div>
-                            ))
+                                        </Link>
+                                    </div>
+                                ))
+                            ) : (
+                                <div className="text-white px-2">No movies found.</div>
+                            )
                         }
                     </div>
                 </div>
